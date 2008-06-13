324679112ff6b51dd8dabf1b33ec69a1ba4cae9c
jeanlou.dupont <jeanlou.dupont@51235a64-d92d-0410-a7aa-c7403dfc0a9e>
2008-06-13 19:08:14 +0000

    QuickEditProtect: first commit

---
+++ trunk/extensions/QuickEditProtect/QuickEditProtect.i18n.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Jean-Lou Dupont
+ * @package QuickEditProtect
+ * @version @@package-version@@
+ * @Id $Id$
+ */
+//<source lang=php>
+
+MW_QuickEditProtect::getInstance('MW_QuickEditProtect')->setMessages(
+	array( 'en' => 
+		array(
+			'quickeditprotect'				=> 'QuickEditProtect',
+			'quickeditprotect-protect'		=> 'Quick Edit Protect',		
+			'quickeditprotect-unprotect'	=> 'Quick Edit UnProtect',
+			'quickeditprotect-help'			=> 'Don\'t forget to add the file <i>QuickEditProtect.js</i> to the local page [[Mediawiki:Common.js]]',				
+			#'' => '',
+		),
+			#other languages
+			#'fr' =>
+	)
+);
+//</source>

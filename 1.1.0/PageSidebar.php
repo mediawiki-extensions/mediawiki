184c63e454b4396417f133c1106827c0cd9022d2
jeanlou.dupont <jeanlou.dupont@51235a64-d92d-0410-a7aa-c7403dfc0a9e>
2008-06-18 00:40:50 +0000

    v1.1.0 of [[Extension:PageSidebar]]

---
+++ 1.1.0/PageSidebar.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Jean-Lou Dupont
+ * @package PageSidebar	
+ * @version $Id: PageSidebar.php 1189 2008-06-18 00:39:55Z jeanlou.dupont $
+ */
+//<source lang=php>
+if (!class_exists('StubManager'))
+	echo '[[Extension:PageSidebar]] <b>requires</b> [[Extension:StubManager]]'."\n";
+else
+{
+	global $wgExtensionCredits;
+	$wgExtensionCredits['other'][] = array( 
+		'name'    		=> 'PageSidebar',
+		'version'		=> '1.1.0',
+		'author'		=> 'Jean-Lou Dupont',
+		'url'			=> 'http://www.mediawiki.org/wiki/Extension:PageSidebar',	
+		'description' 	=> "Provides per-page arbitrary wikitext for the sidebar.", 
+	);
+	
+	StubManager::createStub2(	array(	'class' 		=> 'PageSidebar', 
+										'classfilename'	=> dirname(__FILE__).'/PageSidebar.body.php',
+										'hooks'			=> array( 'SkinTemplateOutputPageBeforeExec', 'OutputPageParserOutput' ),
+										'tags'			=> array( 'pagesidebar' ),
+									)
+							);
+}
+// </source>
